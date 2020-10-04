08756a551836870dacc1ce375e31cbad79e005e1
pocari <caffelattenonsugar@gmail.com>
2020-10-04 16:49:10 +0900

    paragraphを別ファイルに切り出す

---
+++ src/components/article/paragraph.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import styles from './index.module.css';
+
+export const Paragraph: React.FC<{ p: string }> = ({ p }) => {
+  return <div className={styles.paragraph}>{p}</div>;
+};
