6203006128f9a196ff5dfed08cb30a1d16f1521d
pocari <caffelattenonsugar@gmail.com>
2020-10-04 16:39:26 +0900

    contentをArticleに切り出す

--- src/pages/[userId]/[articleId]/index.tsx
+++ src/pages/[userId]/[articleId]/index.tsx
@@ -4,6 +4,7 @@ import Error from 'next/error';
 
 import { useGetArticleQuery } from '@/generated/graphql';
 import styles from './index.module.css';
+import { Article } from '@/components/article';
 
 const ArticlePage: NextPage = () => {
   const router = useRouter();
@@ -40,7 +41,9 @@ const ArticlePage: NextPage = () => {
           <span className={styles.userName}>{user.displayName}</span>
         </div>
       </div>
-      <div className={styles.content}>{content}</div>
+      <div className={styles.content}>
+        <Article content={content} />
+      </div>
     </div>
   );
 };
